bad65e6d56b2c55436b210210ed0070e396c54e6
MaicolD0930 <80848782+MaicolD0930@users.noreply.github.com>
2021-12-27 13:20:27 -0500

    Diseño nuevo1

--- index.php
+++ index.php
@@ -43,6 +43,78 @@ aside{
     height: 32px;
 
 }
+.BtnNormal {
+  display: inline-block;
+  padding: 5px 5px;
+  font-size: 13px;
+  cursor: pointer;
+  text-align: center;	
+  outline: none;
+  color: #fff;
+  background-color: rgb(57, 57, 233);
+  border: 3px Solid transparent;
+  border-radius: 3px;
+
+}
+
+    .BtnNormal:active {
+  background-color: blue;
+  box-shadow: 0 5px #666;
+  transform: translateY(4px);
+}
+
+
+.oculto{
+    display:none;
+}
+
+.OcBtn{
+    background-color: rgb(128, 128, 143);
+    border-radius: 4px;
+    color: white;
+    border: 2px solid black;
+    width: 40px;
+}
+.OcBtn:hover {
+    background-color: black;
+    color: white;
+}
+
+.OcBtn1{
+    background-color: rgb(128, 128, 160);
+    border-radius: 4px;
+    color: white;
+    border: 2px solid black;
+    width: 40px;
+}
+.OcBtn1:hover {
+    background-color: black;
+    color: white;
+}
+
+.OmBtn{
+    background-color: rgb(128, 128, 143);
+    border-radius: 4px;
+    color: red;
+    border: 2px solid black;
+    width: 40px;
+}
+.OmBtn:hover {
+    background-color: black;
+    color: red;
+}
+
+.OmBtn1{
+    background-color: rgb(128, 128, 160);
+    border-radius: 4px;
+    color: red;
+    border: 2px solid black;
+    width: 40px;
+}
+.OmBtn1:hover {
+    background-color: black;
+    color: red;
+}
 
 
 
@@ -60,11 +132,14 @@ aside{
     <article class="col-md-8">
     <h3>Determinar el área de un triángulo</h3>
     <label>Ingrese la base y altura del triangulo.</label>
-    <br>(Tenga en cuenta que para determinar el área de un triangulo la formula base es: Base x Altura dividida 2)<br>
-    <br><br>
+    <button class="OcBtn" id="MostrarInv1">?</button>
+    <button class="OmBtn" id="OcultarInv1">X</button>
+    <p id="Inv1">(Tenga en cuenta que para determinar el área de un triangulo la formula 
+        base es: Base x Altura dividida 2)</p>
+        <p></p>
     <input class="formularioIp" type="number" id="base" placeholder="Ingrese la base">
     <input class="formularioIp" type="number" id="altura" placeholder="Ingrese la altura">
-    <button class="btn btn-primary" onclick="area()">Calcular área</button>
+    <button class="BtnNormal" onclick="area()">Calcular área</button>
     <br><br>
     <label>Área: </label>
     <div id="areaResultado"></div>
@@ -75,6 +150,7 @@ aside{
     </aside>
 </section>
 </div>
+
 <script>
   function area(){
    var areaResultado = document.getElementById("areaResultado");
@@ -101,14 +177,17 @@ aside{
     <article class="col-md-8">
     <h3>Determinar el numero mayor entre 3 imputs</h3>
     <label>Ingrese 3 numeros enteros para determinar el mayor de estos.</label>
-    <br></br>
+    <button class="OcBtn1" id="MostrarInv2">?</button>
+    <button class="OmBtn1" id="OcultarInv2">X</button>
+    <p id="Inv2">(Tenga en cuenta que los números a ingresar deben ser enteros.)</p>
+    <p></p>
 <input class="formularioIp" type="number" id="numero1" placeholder="Ingrese primer numero">
 <input class="formularioIp" type="number" id="numero2" placeholder="Ingrese segundo numero">
 <br></br>
 <input class="formularioIp" type="number" id="numero3" placeholder="Ingrese tercer numero">
 
-<button class="btn btn-primary" onclick="Valor()">Determinar numero mayor</button>
-<br><br>
+<button class="BtnNormal" onclick="Valor()">Determinar numero mayor</button>
+<p></p>
 <label>El numero Mayor es: </label>
 <div id="NumeroM"></div>
     </article>
@@ -199,13 +278,15 @@ aside{
     <article class="col-md-8">
     <h3>Definir que tipo de triangulo es por medio de sus medidas</h3>
     <label>Ingrese los tres lados del triangulo.</label>
-    <br>(El triángulo equilátero se caracteriza por tener todos sus lados iguales. El triángulo isósceles tiene dos lados iguales y un lado desigual. El triángulo escaleno tiene todos sus lados con diferentes longitudes.)</br>
-    <br></br>
+    <button class="OcBtn" id="MostrarInv3">?</button>
+    <button class="OmBtn" id="OcultarInv3">X</button>
+    <p id="Inv3">(El triángulo equilátero se caracteriza por tener todos sus lados iguales. El triángulo isósceles tiene dos lados iguales y un lado desigual. El triángulo escaleno tiene todos sus lados con diferentes longitudes.)</p>
+    <p></p>
 <input class="formularioIp" type="number" id="ladoa" placeholder="Ingrese lado A">
 <input class="formularioIp" type="number" id="ladob" placeholder="Ingrese lado B">
-<br></br>
+<p></p>
 <input class="formularioIp" type="number" id="ladoc" placeholder="Ingrese lado C">
-<button class="btn btn-primary" onclick="triangulo()">Definir el tipo de triangulo</button>
+<button class="BtnNormal" onclick="triangulo()">Definir el tipo de triangulo</button>
 <br><br>
 <label>Triangulo: </label>
 <div id="Ttriangulo">
@@ -253,11 +334,13 @@ aside{
     <article class="col-md-8">
     <h3>Definir si es numero primo</h3>
     <label>Ingrese un número.</label>
-<br>(Los números primos son aquellos números naturales que solamente se pueden dividir por sí mismos y por 1)<br>
-<br></br>
+    <button class="OcBtn1" id="MostrarInv4">?</button>
+    <button class="OmBtn1" id="OcultarInv4">X</button>
+<p id="Inv4">(Los números primos son aquellos números naturales que solamente se pueden dividir por sí mismos y por 1)</p>
+<p></p>
 <input class="formularioIp" type="number" id="numero" placeholder="Ingrese un número">
 
-<button class="btn btn-primary" onclick="primoo()">Determinar número</button>
+<button class="BtnNormal" onclick="primoo()">Determinar número</button>
 <br></br>
 <label>Numero primo: </label>
 <div id="nnPrimo">
@@ -305,10 +388,12 @@ aside{
 
     <h3>Determinar salario de un trabajador</h3>
     <label>Ingrese la cantidad de horas que trabajo en el mes.</label>
-
-    <p>(Tenga en cuenta que esta establecido que un mes cuenta con 30 días, y 255 horas de trabajo, cada hora extra se pagara un 25% mas.)</p>
+    <button class="OcBtn" id="MostrarInv5">?</button>
+    <button class="OmBtn" id="OcultarInv5">X</button>
+    <p id="Inv5">(Tenga en cuenta que esta establecido que un mes cuenta con 30 días, y 255 horas de trabajo, cada hora extra se pagara un 25% mas.)</p>
+    <p></p>
     <input class="formularioIp" type="float" id="horas" placeholder="Ingrese horas trabajadas">
-    <button class="btn btn-primary" onclick="Hextra()">Determinar pago del mes</button>
+    <button class="BtnNormal" onclick="Hextra()">Determinar pago del mes</button>
     <p></p>
     <label>Su pago es: </label>
     <div id="Hextraa"></div>
@@ -349,5 +434,103 @@ aside{
 
 <script src="js/jquery.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
+
+<script>
+$(document).ready(() => {
+        $('#Inv1').hide();
+        $('#Inv2').hide();
+        $('#Inv3').hide();
+        $('#Inv4').hide();
+        $('#Inv5').hide();
+        $('#OcultarInv1').hide();
+        $('#OcultarInv2').hide();
+        $('#OcultarInv3').hide();
+        $('#OcultarInv4').hide();
+        $('#OcultarInv5').hide();
+
+        $('#MostrarInv1').click(function() {
+            $('#MostrarInv1').hide();  
+            $('#OcultarInv1').show();      
+        });
+        $('#OcultarInv1').click(function() {
+            $('#MostrarInv1').show();  
+            $('#OcultarInv1').hide();      
+        });
+
+        $('#MostrarInv2').click(function() {
+            $('#MostrarInv2').hide();  
+            $('#OcultarInv2').show();      
+        });
+        $('#OcultarInv2').click(function() {
+            $('#MostrarInv2').show();  
+            $('#OcultarInv2').hide();      
+        });
+
+        $('#MostrarInv3').click(function() {
+            $('#MostrarInv3').hide();  
+            $('#OcultarInv3').show();      
+        });
+        $('#OcultarInv3').click(function() {
+            $('#MostrarInv3').show();  
+            $('#OcultarInv3').hide();      
+        });
+        $('#MostrarInv4').click(function() {
+            $('#MostrarInv4').hide();  
+            $('#OcultarInv4').show();      
+        });
+        $('#OcultarInv4').click(function() {
+            $('#MostrarInv4').show();  
+            $('#OcultarInv4').hide();      
+        });
+        $('#MostrarInv5').click(function() {
+            $('#MostrarInv5').hide();  
+            $('#OcultarInv5').show();      
+        });
+        $('#OcultarInv5').click(function() {
+            $('#MostrarInv5').show();  
+            $('#OcultarInv5').hide();      
+        });
+
+
+
+
+
+        $('#OcultarInv1').click(function() {
+            $('#Inv1').hide();      
+        });
+        $('#OcultarInv2').click(function() {
+            $('#Inv2').hide();      
+        });
+        $('#OcultarInv3').click(function() {
+            $('#Inv3').hide();
+        });
+        $('#OcultarInv4').click(function() {
+            $('#Inv4').hide();
+        });
+        $('#OcultarInv5').click(function() {
+            $('#Inv5').hide();
+        });
+
+
+        $('#MostrarInv1').click(function() {
+            $('#Inv1').show();      
+        });
+        $('#MostrarInv2').click(function() {
+            $('#Inv2').show();      
+        });
+        $('#MostrarInv3').click(function() {
+            $('#Inv3').show();
+        });
+        $('#MostrarInv4').click(function() {
+            $('#Inv4').show();
+        });
+        $('#MostrarInv5').click(function() {
+            $('#Inv5').show();
+        });
+
+
+    });
+</script>
 </body>
 </html>
